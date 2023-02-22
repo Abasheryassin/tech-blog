51f898cdb411eca6eb79062bf6ea6e33b76ac6c2
abasheryassin <abasheryassin@gmail.com>
2023-02-22 12:59:13 -0600

    comment delete and put routes finished

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,4 +15,47 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.update({
+            description: req.body.description,
+        },
+        {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
