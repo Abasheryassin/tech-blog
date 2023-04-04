c4becb3310066de1b69d17ab01f0b2e71c36256f
abasheryassin <abasheryassin@gmail.com>
2023-04-03 19:33:25 -0500

    home page loading

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,11 +14,12 @@ router.get('/', async (req, res) => {
     });
 
     const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
-
-    res.render('homepage', {
-      blogPosts,
-      logged_in: req.session.logged_in,
-    });
+ 
+    res.render('homepage', 
+   {
+    blogPosts,
+    logged_in: req.session.logged_in
+   });
 
   } catch (err) {
     res.status(500).json(err);
